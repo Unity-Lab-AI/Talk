3c70a5e6251a92677e29f48ea102c1b383a8114c
G-Fourteen <gfourteen7525@gmail.com>
2025-11-03 01:33:24 -0700

    Delete tests directory

--- tests/content_integrity_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if ! grep -q "<title>" index.html; then
-  echo "index.html is missing a <title> element"
-  exit 1
-fi
-
-echo "Verified that index.html contains a <title> element."
